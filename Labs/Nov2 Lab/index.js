3380fe1804813665fbddcdd9c0310e8223ad677e
AlanJschultz <alanjschultz@gmail.com>
2019-11-04 17:47:59 -0500

    11042019-1747

---
+++ Labs/Nov2 Lab/index.js
@@ -0,0 +1,14 @@
+
+/*
+
+HTML / CSS Exercise:
+Using an html file with a linked css file, make a brief page(no header or footer needed) which explains the 2 things that help you greatly in learning code and the 2 things you struggle with mightily in learning code.These can be websites, books, articles, etc.Feel free to design this however you want, but use your time(45 minutes) to build this out and have fun.We 'll use the 15 minutes following this exercise to show off some of your work!
+
+Create this sample webpage in a new project folder within a projects parent directory.
+Grab an animation you like from this webpage(Links to an external site.): (http: //www.justinaguilar.com/animations/index.html#}
+
+Once you 've finished working on this page, save your webpage as a code pen and submit the URL on Canvas. Include the animation in your page.
+
+
+*/
+
