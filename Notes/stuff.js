a2feabc4064d65d7b161e7d637279eefdb6ec11f
AlanJschultz <alanjschultz@gmail.com>
2019-11-06 14:22:08 -0500

    as of end of 11052019.

---
+++ Notes/stuff.js
@@ -0,0 +1,7 @@
+//jshint esversion:7
+
+
+let newNumber = 5;
+console.log("This is a single typeof of a number you get", typeof newNumber);
+console.log("What happens when you do 2 typeof in a row ", typeof typeof newNumber);
+
