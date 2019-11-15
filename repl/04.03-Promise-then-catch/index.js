c81fee71227c993407d693833ac06395d763c604
AlanJschultz <alanjschultz@gmail.com>
2019-11-15 17:22:49 -0500

    repls 4-3 Promise-then-catch

--- repl/04.03-Promise-then-catch/index.js
+++ repl/04.03-Promise-then-catch/index.js
@@ -17,3 +17,27 @@ resolved or rejected promise and print a message.
 
 */
 
+let splitSecondTime = Date.now();
+
+// console.log(splitSecondTime);
+// if (splitSecondTime % 2 == 0) {
+//     console.log('Millisecond Time is Even');
+// } else {
+//     console.log('Millisecond Time is Odd');
+// }
+
+let milliseconds = new Promise((resolve, reject) => {
+    if (splitSecondTime % 2 == 0) {
+        resolve('resolved promise even time');  // comes up with the resolved value
+    } else {
+        reject('rejected promise odd time');  // comes up with the rejected error
+    }
+});
+
+milliseconds
+    .then((value) => console.log(splitSecondTime, value))  // function for resolved
+    .catch((error) => console.log(splitSecondTime, error));  // function for rejected
+
+
+// 1573855637191 "rejected promise odd time"
+// 1573856008286 "resolved promise even time"
